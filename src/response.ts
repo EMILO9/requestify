fa74bf362f085833883a9af85bd246425d90f7f7
Emil Berberg Olsen <emil@NETT-OPP.local>
2026-09-21 14:54:29 +0200

    d

--- src/response.ts
+++ src/response.ts
@@ -1,7 +1,21 @@
 import type { ServerResponse } from "node:http";
 
 export default function response(res: ServerResponse) {
-  return { raw: res };
+  return {
+    raw: res,
+    status(code: number) {
+      res.statusCode = code;
+      return this;
+    },
+    json(data: any) {
+      res.setHeader("Content-Type", "application/json; charset=utf-8");
+      res.end(JSON.stringify(data));
+    },
+    send(body: string | Buffer) {
+      res.setHeader("Content-Type", "text/plain; charset=utf-8");
+      res.end(body);
+    },
+  };
 }
 
 export type Response = ReturnType<typeof response>;
