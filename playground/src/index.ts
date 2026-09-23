ce079e3d2f0c7191774a68c6bf8cce7da92b274b
Emil Berberg Olsen <emil@NETT-OPP.local>
2026-09-23 16:04:27 +0200

    D

--- playground/src/index.ts
+++ playground/src/index.ts
@@ -3,20 +3,16 @@ import Requestify from "@emilo/requestify";
 const app = Requestify({
   namespaces: {
     "/": {
-      middleware: [],
       routes: [
         {
-          path: "/",
+          path: "/:slug",
           methods: ["GET"],
-          handler: ({ req, res }) => {
-            throw new Error("Some error!");
-            res.status(200).json({
-              path: req.path,
-              params: req.params,
-              query: req.query,
-              cookies: req.cookies,
-            });
-          },
+          handler: ({ req, res }) => res.status(200).send(req.path),
+        },
+        {
+          path: "/{*splat}",
+          methods: ["GET"],
+          handler: ({ req, res }) => res.status(200).send("*"),
         },
       ],
     },
